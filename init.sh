cc4b9831883955ffd335b3ee3b4a9ee9ebb73e6c
abr-ya <gamer05@list.ru>
2017-03-14 18:24:24 +0300

    new init

--- init.sh
+++ init.sh
@@ -1,7 +1,2 @@
-#!/usr/bin/env bash
-
-ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-/etc/init.d/nginx restart
-
-gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
